392e9ae521317cb10fe229304f82ee1f1422c561
bgpat <bgpat@bgpat.net>
2014-12-05 11:22:52 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+/**
+ * @file Object Oriented Programming Library for Javascript
+ *
+ * @author bgpat <bgpat@bgpat.net>
+ * @license MIT
+ * @version 1.0, 2014-12-03
+ */
+
+/** @namespace */
+var oop = {
+  /**
+   * inherit super class
+   * @param {Function} child sub class
+   * @param {Function} parent super class
+   * @example oop.extend(ChildClass, ParentClass);
+   */
+  extend: function (child, parent) {
+    child.prototype = Object.create(
+      parent.prototype,
+      { constructor: { value: child } }
+    );
+  },
+
+  /**
+   * define member variables
+   * @param {Object} obj target object
+   * @param {Object} members member list
+   * @example oop.member(Foo.prototype, {
+   *    foo: 'foo is public member',
+   *    _bar: '_bar is private member',
+   *  });
+   */
+  member: function (obj, members) {
+    for (var name in members) {
+      Object.defineProperty(obj, name, {
+        enumerable: name.charAt(0) !== '_',
+        value: members[name],
+        writable: true,
+      });
+    }
+  },
+
+  /**
+   * define properties
+   * @param {Object} obj target object
+   * @param {Object.<oop.Property>} properties property list
+   * @example oop.property(Foo.prototype, {
+   *    foo: {
+   *      get: function () { return 'foo is public property'; },
+   *      set: function (value) { this._foo = value; },
+   *    },
+   *    _bar: {
+   *      get: function () { return '_bar is private and readonly property',
+   *    }
+   *  });
+   */
+  property: function (obj, properties) {
+    for (var name in properties) {
+      Object.defineProperty(obj, name, {
+        enumerable: name.charAt(0) !== '_',
+        get: properties[name].get,
+        set: properties[name].set,
+      });
+    }
+  },
+
+  /**
+   * define methods
+   * @param {Object} obj target object
+   * @param {Object.<Function>} methods method list
+   * @example oop.method(Foo.prototype, {
+   *    foo: function () { return 'foo is a public method'; },
+   *    _bar: function () { return 'bar is a private method'; },
+   *  });
+   */
+  method: function (obj, methods) {
+    for (var name in methods) {
+      Object.defineProperty(obj, name, {
+        enumerable: name.charAt(0) !== '_',
+        value: methods[name],
+      });
+    }
+  },
+
+  /**
+   * @typedef {Object.<Function>} oop.Property
+   * @property {Function} [get] getter
+   * @property {Function} [set] setter
+   */
+};
+
+module.exports = oop;
+
---
+++ test.js
@@ -0,0 +1,43 @@
+/**
+ * @file test.js
+ *
+ * @author bgpat <bgpat@bgpat.net>
+ * @license MIT
+ * @version 1.0, 2014-12-03
+ */
+
+var oop = require('./index.js');
+
+var Hoge = function () {};
+var Fuga = function () {};
+
+oop.extend(Hoge, Fuga);
+
+oop.member(Hoge.prototype, {
+  hoge: 'hoge',
+  _hoge: '_hoge',
+});
+
+oop.property(Fuga.prototype, {
+  fuga: {
+    get: function () { return 'fuga'; },
+  },
+  _fuga: {
+    get: function () { return '_fuga'; },
+  },
+});
+
+oop.method(Hoge.prototype, {
+  piyo: function () {
+    return 'piyo';
+  },
+  _piyo: function () {
+    return '_piyo';
+  },
+});
+
+var hoge = new Hoge();
+for (var key in hoge) {
+  console.log(key, hoge[key]);
+}
+
